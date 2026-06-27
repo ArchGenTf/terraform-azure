4350b77d0505c6a42e514d5796c38b7daf22333f
praveeneswarM <praveeneswarmadhes@gmail.com>
2026-06-27 12:32:11 +0530

    infra: delete unused modules/servicebus directory

--- modules/servicebus/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "servicebus_namespace_name" {
-  value = azurerm_servicebus_namespace.sb.name
-}
-
-output "servicebus_primary_connection_string" {
-  value     = azurerm_servicebus_namespace_authorization_rule.rule.primary_connection_string
-  sensitive = true
-}
